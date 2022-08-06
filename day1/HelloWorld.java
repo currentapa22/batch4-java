0df605ebbe8d7a6d53ea2d1409d987d48369f439
currentapa22 <110708638+currentapa22@users.noreply.github.com>
2022-08-06 16:59:15 -0500

    Add files via upload

---
+++ day1/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.days.day1;
+
+import com.days.day21.Methods;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Methods.burak ();
+
+        System.out.println("        Hello World     ");
+        System.out.println(123456789);
+        System.out.println("Merhaba " + 2 + " kisi");
+
+
+        System.out.println ("Tekrar ettigim icin not ekliyorum!! 5/28/22 4.30 pm");
+
+
+
+
+
+
+
+
+     }
+
+}
